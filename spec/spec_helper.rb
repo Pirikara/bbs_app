43eed5232868f3ae7237c8e5ceeb4c9f8be90c3f
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-18 09:49:56 +0900

    database_cleaner gemの導入

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,13 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # テスト実行時に
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+    Rails.application.load_seed
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
