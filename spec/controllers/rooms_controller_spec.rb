b84840e08044b9ee76abc2114e15ad342479a6af
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-18 09:12:07 +0900

    掲示板の詳細画面

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe RoomsController, type: :controller do
+describe RoomsController, type: :controller do
+  let(:user) { create(:user) }
+
+  describe 'GET #new' do
+    context 'log in' do
+      before do
+        login user
+      end
+      it "投稿ページを表示する" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+    context 'not log in' do
+      it 'ログインしていない場合、トップページにリダイレクトされる' do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 
 end
