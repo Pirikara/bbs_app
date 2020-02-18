94324004bcb199aefb681a50ce9125b3110ed831
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-18 16:51:09 +0900

    マイページのview作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @host_rooms = Room.where(host_id: @user.id)
+    messages = Message.where(user_id: @user.id)
+    room_ids = []
+    messages.each do |m|
+      room_ids << m.room_id
+    end
+
+    r_ids = room_ids.uniq
+    @gest_rooms = Room.where(id: r_ids)
+  end
+end
