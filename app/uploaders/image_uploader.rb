b87f0db1b579205d8ded024a8a36cbf0108ca76c
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-19 19:12:41 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # if Rails.env.development? || Rails.env.test?
   #   storage :file
   # else
-  storage :fog
+  storage :file
   # end
   # storage :fog
 
