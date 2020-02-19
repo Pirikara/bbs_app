73b7c07bf7664304b91796183bdd9068dd1b6e9f
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-19 23:20:29 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # if Rails.env.development? || Rails.env.test?
-  #   storage :file
+    storage :file
   # else
-  storage :file
+  #  storage :fog
   # end
   # storage :fog
 
